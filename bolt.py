12bf4dd569f60c5bc51963501d3aa4dca7b376ee
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-07-30 15:17:03 +0530

    commit 2

--- bolt.py
+++ bolt.py
@@ -7,8 +7,23 @@ A_KEY = input("API key:")
 mybolt = Bolt(A_KEY, D_ID)
 
 while True:
-    statmesg = mybolt.isOnline()
-    status = json.loads(statmesg)
-    if status['success']:
-        temp = mybolt.analogRead('A0')
+    try:
+        statmesg = mybolt.isOnline()
+        status = json.loads(statmesg)
+        if status['value'] == 'offline':
+            print("Device is offline!")
+            break
+        resp = mybolt.analogRead('A0')
+        tempd = json.loads(resp)
+        if not tempd['success']:
+            print("Request unsucessfull!")
+            break
+        temp = int(tempd['value'])/10.24
+        print(temp)
+        mybolt.digitalWrite(1, 'HIGH')
+        time.sleep(5)
+        mybolt.digitalWrite(1, 'LOW')
+    except Exception as e:
+        print(e)
+        break
     time.sleep(10)
