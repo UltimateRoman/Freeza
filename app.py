d1c5ad83b8a3e7cb5c64c1cece2bf6aa2033da48
Athul Sanjose <59477486+UltimateRoman@users.noreply.github.com>
2020-08-01 18:26:44 +0530

    bot improvement

--- app.py
+++ app.py
@@ -3,17 +3,46 @@ import time, requests, json
 token=input("Enter telegram bot token:")
 url = "https://api.telegram.org/bot"+token
 
-def get_message():
-    resp = requests.get(url+"/getUpdates")
+def get_message(offset):
+    if offset:
+        resp = requests.get(url+"/getUpdates?offset={}".format(offset))
+    else:
+        resp = requests.get(url+"/getUpdates")
     respd = json.loads(resp.content.decode("utf8"))
-    new_mesg = len(respd['result'])-1
-    text = respd['result'][new_mesg]["message"]["text"]
-    chat_id = respd['result'][new_mesg]["message"]["chat"]["id"]
-    return (text, chat_id)
+    return respd
+
+def get_last_upid(respd):
+    upids=[]
+    for msg in respd['result']:
+        upids.append(int(msg['update_id']))
+    return max(upids)
+
+def make_msg(respd):
+    for msg in respd['result']:
+        try:
+            text = msg['message']['text']
+            chat_id = msg['message']['chat']['id']
+            send_message(text, chat_id)
+        except Exception as e:
+            print(e)
 
 def send_message(text, chat_id):
     resp = requests.get(url+"/sendMessage?text={}&chat_id={}".format(text, chat_id))
+    rem = json.loads(resp.content.decode("utf8"))
+    print("Replied to:", rem['result']['chat']['first_name'])
 
-text, chat_id = get_message()
-send_message(text, chat_id)
-    
+
+def main():
+    last_upid = None
+    while True:
+        try:
+            respd = get_message(last_upid)
+            if len(respd['result'])>0:
+                last_upid = get_last_upid(respd)+1
+                make_msg(respd)
+            time.sleep(1)
+        except Exception as e:
+            print(e)
+
+if __name__ == '__main__':
+    main()
